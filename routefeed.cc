eb94bf6df93f6798daa4a163205e80c83f91a499
Maho Morichika <nat@magicnat.com>
2019-08-02 00:53:57 +0800

    routefeed: cli & help

--- routefeed.cc
+++ routefeed.cc
@@ -1,5 +1,7 @@
 #include "feeder.h"
 #include <arpa/inet.h>
+#include <signal.h>
+#include <unistd.h>
 
 using namespace cnrf;
 
@@ -10,16 +12,57 @@ void handle_sig(__attribute__((unused)) int sig) {
     if (feeder != nullptr) feeder->stop();
 }
 
-int main () {
+void help() {
+    fprintf(stderr, "usage: routefeed [-l HOST] [-p PORT] [-t INTERVAL] -a ASN -i BGP_ID -n NEXTHOP\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "cn-routefeed is a BGP speaker that feeds all China IPv4 delegations to peer. \n");
+    fprintf(stderr, "Delegation information are fetch from APNIC.\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "required arguments:\n");
+    fprintf(stderr, "  -a ASN       Local ASN of the BGP speaker.\n");
+    fprintf(stderr, "  -i BGP_ID    BGP ID (Router ID) of the BGP speaker.\n");
+    fprintf(stderr, "  -n NEXTHOP   Nexthop to use when sending routes.\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "optional arguments:\n");
+    fprintf(stderr, "  -l HOST      IP address to bind the BGP speaker on. (default: 0.0.0.0)\n");
+    fprintf(stderr, "  -p PORT      TCP port number to bind the BGP speaker on. (default: 179)\n");
+    fprintf(stderr, "  -t INTERVAL  Time in second to wait between fetching update from APNIC.\n");
+    fprintf(stderr, "               (default: 86400)\n");
+}
+
+int main (int argc, char **argv) {
+    in_addr_t host = INADDR_ANY;
+    in_port_t port = 179;
+    uint32_t asn = 0;
+    uint32_t nexthop = 0;
+    uint32_t router_id = 0;
+    uint32_t interval = 86400;
+
+    char opt;
+
+    while ((opt = getopt(argc, argv, "l:p:a:i:n:t:")) != -1) {
+        switch (opt) {
+            case 'l': host = inet_addr(optarg); break;
+            case 'p': port = atoi(optarg); break;
+            case 'a': asn = atoi(optarg); break;
+            case 'i': inet_pton(AF_INET, optarg, &router_id); break;
+            case 'n': inet_pton(AF_INET, optarg, &nexthop); break;
+            case 't': interval = atoi(optarg); break;
+            default: 
+                help();
+                return 1;
+        }
+    }
+
+    if (asn == 0 || nexthop == 0 || router_id == 0) {
+        help();
+        return 1;
+    }
+
     signal(SIGINT, handle_sig);
     signal(SIGTERM, handle_sig);
 
-    uint32_t router_id;
-    uint32_t nexthop;
-    inet_pton(AF_INET, "172.31.0.1", &router_id);
-    inet_pton(AF_INET, "172.31.0.1", &nexthop);
-
-    Feeder feeder(65000, router_id, nexthop, 3600); 
+    Feeder feeder(asn, router_id, nexthop, interval, host, port); 
     ::feeder = &feeder;
 
     feeder.start();
